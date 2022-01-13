040897a103b8705c31f8674ab3d3bdd236cbabdd
Mario Cardoso <31556314+Maavcardoso@users.noreply.github.com>
2022-01-13 16:54:57 -0300

    Update flappy.js

--- flappy.js
+++ flappy.js
@@ -1,18 +1,23 @@
 
 const conteudo = document.querySelector('div[wm-flappy]')
 
+
 // *** Controla criação dos obstaculos ***
-
 function obsGenerator(){
-    // Cria conjunto de canos
-        const obstaculo = document.createElement('div')
 
+    // Cria o objeto.
+        const obstaculo = document.createElement('div')
         obstaculo.setAttribute('id','obstaculo')
         conteudo.appendChild(obstaculo)
 
+    // Cria conjunto de canos (Divs)
         const canoCima = document.createElement('div')
+        canoCima.setAttribute('id','canoCima')
+        obsHeight = obstaculo.getBoundingClientRect()
+        console.log('obsHeight',obsHeight.height)
+        console.log('obsY', obsHeight.y)
         const canoBaixo = document.createElement('div')
-
+        canoBaixo.setAttribute('id','canoBaixo')
         canoCima.classList.add('cano')
         canoBaixo.classList.add('cano')
         obstaculo.appendChild(canoCima)
@@ -20,11 +25,18 @@ function obsGenerator(){
     // Controla aleatoriedade dos canos
         let randomNum = (Math.floor(Math.random() * 13) + 4 ) * 5
         canoCima.style.height = randomNum + '%'
+        let canoCimaY = canoCima.getBoundingClientRect()
+        console.log('canoCimaHeight', canoCimaY.height + canoCimaY.y)
+
+        //console.log('canoCimaY', canoCimaY.top)
 
     // Se o cano de cima for maior que 80% da altura, cria apenas um gap
         if(randomNum < 80){
             obstaculo.appendChild(canoBaixo)
             canoBaixo.style.height =  (80 - randomNum )  + '%'
+            let canoBaixoY = canoBaixo.getBoundingClientRect()
+            console.log('canoBaixoHeight', canoBaixoY.y)
+           // console.log('canoBaixoY', canoBaixoY.top)
         } 
     // Seta obstáculo p/ a animação.
         obstaculo.style.left = '100%'
@@ -34,10 +46,12 @@ function obsGenerator(){
 
 // Controla o movimento / destruição dos obstáculos
 function obsTime(div){
-var obsPos = 100
-var obsInterval = setInterval(function obsMove(){
+
+let obsPos = 100
+let obsInterval = setInterval(function obsMove(){
     obsPos = obsPos - 0.1
     div.style.left = obsPos + '%' 
+    obsX = div.getBoundingClientRect()
     if(obsPos < -15){
         clearInterval(obsInterval)
         conteudo.removeChild(div)
@@ -48,50 +62,66 @@ var obsInterval = setInterval(function obsMove(){
 // Controla quantos obstáculos são criados
 function obsGenRepeater () {
     obsGenerator()
-    setInterval(()=>{ 
+let obsGeneratorInterval = setInterval(()=>{ 
         obsGenerator()
-    }, 3000)
+    
+    }, 15000)
 };
 obsGenRepeater();
 
-
+// Cria o passáro
 const player = document.createElement('img')
-
 player.setAttribute('id','player')
 conteudo.appendChild(player)
 document.getElementById('player').src = 'imgs/passaro.png'
+playerMove(player)
+
+// Controla movimento do jogador
+function playerMove(player){
+
+    let playerTop = 50
+    let gravity;
+    let playerMoveCheck = false   
+
+    document.addEventListener('keydown',()=>{
+        playerMoveCheck = true
+        if(playerTop > 2){
+            playerTop -= 8
+            player.style.top = (playerTop) + '%';
+        }
+    })
+
+    document.addEventListener('keyup', ()=>{
+        playerMoveCheck = false
+    })
 
 
-
-
-let playerTop = 50
-var gravity;
-var playerMoveCheck = false
-
-
-document.addEventListener('keydown',()=>{
-    playerMoveCheck = true
-    if(playerTop > 2){
-    playerTop -= 8
-    player.style.top = (playerTop) + '%';
-    }
-})
-document.addEventListener('keyup', ()=>{
-    playerMoveCheck = false
-})
-
-
-var gravityCallback = function(){
-    if(playerMoveCheck === false)
-    setInterval(()=>{
-        if(playerTop < 95)
-        playerTop += 0.5
-        player.style.top = (playerTop) + '%'
-},20)
-    else{
-    clearInterval(gravity)
-    }
+    var gravityCallback = function(){
+        if(playerMoveCheck === false)
+        setInterval(()=>{
+            if(playerTop < 95)
+            playerTop += 0.3
+            player.style.top = (playerTop) + '%'
+        },10)
+        else{
+            clearInterval(gravity)
+        }
+    };
+    gravityCallback()
 };
-  gravityCallback()
+
+// Colisão dos objetos
+
+var boxCollision = setInterval(()=>{
+     console.log(player.y)
+ },1000)
 
 
+
+// Toca a música
+// let audio = new Audio('audio/flappysong.mp3');
+// this.onload = function playMusic(){
+//    let audioLoop = setInterval(()=>{
+//         audio.play()
+//     }, 1)
+// };
